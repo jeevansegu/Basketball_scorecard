cf1c7f1b9bdf318b5d4913d143cec94c3bab33ff
jeevansegu <jeevansegu03@gmail.com>
2023-02-19 10:17:45 +0530

    added files

---
+++ index.js
@@ -0,0 +1,55 @@
+let score1El=document.getElementById("score1")
+let score2El=document.getElementById("score2")
+let score1=0
+let score2=0
+function add11() {
+    score1+=1
+    score1El.textContent=score1
+    colorchange()
+}
+function add21() {
+    score1+=2
+    score1El.textContent=score1
+    colorchange()
+}
+function add31() {
+    score1+=3
+    score1El.textContent=score1
+    colorchange()
+}
+function add12() {
+    score2+=1
+    score2El.textContent=score2
+    colorchange()
+}
+function add22() {
+    score2+=2
+    score2El.textContent=score2
+    colorchange()
+}
+function add32() {
+    score2+=3
+    score2El.textContent=score2
+    colorchange()
+}
+function newGame() {
+    score1El.textContent=0
+    score2El.textContent=0
+}
+
+function colorchange() {
+    if (score1>score2) {
+        document.getElementById("home").style.color="#F59E0B"
+        document.getElementById("guest").style.color="#EEEEEE"
+    }
+    else if(score2>score1)
+    {
+        document.getElementById("guest").style.color="#F59E0B"
+        document.getElementById("home").style.color="#EEEEEE"
+    }
+    else
+    {
+        document.getElementById("home").style.color="#EEEEEE"
+        document.getElementById("guest").style.color="#EEEEEE"
+    }
+}
